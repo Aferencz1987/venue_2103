30470191114f50f9fdb261febe6e2ea936f91b36
Alex Ferencz <alex.ferencz6@gmail.com>
2021-03-24 14:58:27 -0600

    Alex F IC 1

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,2 +1,32 @@
 class Venue
+  attr_reader :name, :capacity, :patrons
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @patrons = []
+  end
+
+  def add_patron(patron)
+    @patrons.push(patron)
+  end
+
+  def yell_at_patrons
+    @patrons.map do |patron|
+      patron.upcase
+    end
+  end
+
+  def over_capacity?
+    if @patrons.count <= @capacity
+      false
+    else @patrons.count > @capacity
+      true
+    end
+  end
+
+  def kick_out
+    until @patrons.count <= @capacity
+      @patrons.pop
+    end
+  end
 end
